607060e05143ab95399bb38bbbd2e57ba0b8a6c3
Suhyun Caroline Lee <37642778+suroline@users.noreply.github.com>
2018-06-08 15:48:26 +0900

    Add files via upload

---
+++ javascript_camp/0608.js
@@ -0,0 +1,22 @@
+
+// Complete a function that returns the maximum and minimum values of elements in the array.
+// (배열의 요소 중 최대값, 최소값을 반환하는 함수를 완성하라)
+
+function getMaxValueFromArray(array) {
+
+  return Math.max.apply(null, array);
+  // ES6: Math.max(...array); ===> spead tech
+  
+}
+
+console.log(getMaxValueFromArray([3, 6, -2, -5, 7, 3])); // 7
+
+
+function getMinValueFromArray(array) {
+  
+  return Math.min.apply(null, array);
+  // ES6: Math.min(...array); ===> spead tech
+
+}
+
+console.log(getMinValueFromArray([3, 6, -2, -5, 7, 3])); // -5
